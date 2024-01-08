e1cd62ff6fd34a5f239b1c6962f1b4840ffb7b7d
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-01-08 21:47:48 +0100

    add Ticket Model

--- LITRevu/review/models.py
+++ LITRevu/review/models.py
@@ -6,6 +6,16 @@ from django.conf import settings
 from django.db import models
 
 
+class Ticket(models.Model):
+    # Your Ticket model definition goes here
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
 class Review(models.Model):
     ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
