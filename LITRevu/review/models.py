021acc79f754eecc0e101a85c35929e1fba93444
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-01-08 21:47:29 +0100

    project creation with Review Model

---
+++ LITRevu/review/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
+from django.db import models
+
+
+class Review(models.Model):
+    ticket = models.ForeignKey('Ticket', on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
