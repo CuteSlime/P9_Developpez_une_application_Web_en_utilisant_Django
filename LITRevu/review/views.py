8f2dea1283de081b621ddf0803972b3f49c23a7d
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-01-10 12:47:10 +0100

    change view to class based view

--- LITRevu/review/views.py
+++ LITRevu/review/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class Index(TemplateView):
+    template_name = "review/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["back_url"] = reverse_lazy("index")
+
+        return context
