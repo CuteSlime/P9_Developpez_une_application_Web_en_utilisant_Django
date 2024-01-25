561f700334b13b299dd1bafebc6f481e8485006c
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-01-25 14:47:51 +0100

    add review model to admin

--- LITRevu/review/admin.py
+++ LITRevu/review/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from review.models import Ticket, Review, UserFollows
 
 # Register your models here.
+
+
+class TicketAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    # liste les champs que nous voulons sur l'affichage de la liste
+    list_display = ('title', 'user', 'time_created')
+
+
+class ReviewAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    # liste les champs que nous voulons sur l'affichage de la liste
+    list_display = ('headline', 'ticket', 'rating', 'user', 'time_created')
+
+
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollows)
