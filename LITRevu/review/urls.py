a120bce528784ca9379602c965e56f943964cdec
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-01-26 11:26:49 +0100

    update view and url for CRUD

--- LITRevu/review/urls.py
+++ LITRevu/review/urls.py
@@ -1,8 +1,36 @@
 from django.urls import path
-from .views import Flux, Ticket, Ticket_review
-urlpatterns = [
-    path("flux/", Flux.as_view(), name="flux"),
-    path("ticket/", Ticket.as_view(), name="ticket"),
-    path("ticket_review/", Ticket_review.as_view(), name="ticket_review"),
+from .views import (
+    TicketListView,
+    TicketCreateView,
+    TicketDetailView,
+    TicketUpdateView,
+    TicketDeleteView,
+    TicketReviewCreateView,
+    ReviewCreateView,
+    ReviewUpdateView,
+    ReviewDeleteView,
+)
 
+urlpatterns = [
+    path("flux/", TicketListView.as_view(), name="flux"),
 ]
+
+# Ticket
+urlpatterns += (
+    path("ticket/create/", TicketCreateView.as_view(), name="ticket"),          # C
+    path("ticket/<int:pk>/", TicketDetailView.as_view(), name="ticket"),        # R
+    path("ticket/update/<int:pk>/", TicketUpdateView.as_view(), name="ticket"),  # U
+    path("ticket/delete/<int:pk>/", TicketDeleteView.as_view(), name="ticket"),  # D
+)
+# create and review a ticket at the same time
+urlpatterns += (
+    path("ticket_review/create/",
+         TicketReviewCreateView.as_view(), name="ticket_review"),
+
+)
+# Review
+urlpatterns += (
+    path("review/create/", ReviewCreateView.as_view(), name="review"),
+    path("review/update/<int:pk>/", ReviewUpdateView.as_view(), name="review"),
+    path("review/delete/<int:pk>/", ReviewDeleteView.as_view(), name="review"),
+)
--- LITRevu/review/views.py
+++ LITRevu/review/views.py
@@ -1,17 +1,73 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .models import Ticket, Review
+
+# Flux
 
 
-class Flux(TemplateView):
+class TicketListView(ListView):
+    model = Ticket
     template_name = "review/flux.html"
 
 
-class Ticket(TemplateView):
+class TicketCreateView(CreateView):
+    model = Ticket
+    template_name = "review/ticket_create.html"
+
+# Ticket
+
+
+class TicketDetailView(DetailView):
+    model = Ticket
     template_name = "review/ticket.html"
 
 
-class Ticket_review(TemplateView):
-    template_name = "review/ticket_review.html"
+class TicketUpdateView(UpdateView):
+    model = Ticket
+    fields = (
+        "title",
+        "description",
+        "image",
+    )
+    template_name = "review/ticket_update.html"
+
+
+class TicketDeleteView(DeleteView):
+    model = Ticket
+    template_name = "review/ticket_delete.html"
+    success_url = reverse_lazy("flux")
+
+# create and review a ticket at the same time
+
+
+class TicketReviewCreateView(CreateView):
+    template_name = "review/ticket_review_create.html"
+
+
+# review
+class ReviewCreateView(CreateView):
+    template_name = "review/review_create.html"
+
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = "review/flux.html"
+
+
+class ReviewUpdateView(UpdateView):
+    model = Review
+    fields = (
+        "title",
+        "description",
+        "image",
+    )
+    template_name = "review/review_update.html"
+
+
+class ReviewDeleteView(DeleteView):
+    model = Review
+    template_name = "review/review_delete.html"
+    success_url = reverse_lazy("flux")
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
