3233fc4c6fccf3c8e4d359616c1eec8cdaea7a6f
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-02-01 13:04:17 +0100

    Update MEDIA setting

--- LITRevu/LITRevu/urls.py
+++ LITRevu/LITRevu/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from accounts.views import HomeView
 
@@ -22,10 +24,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", HomeView.as_view(), name="home"),
 ]
-
 urlpatterns += (
     path("__reload__/", include("django_browser_reload.urls")),
     path("review/", include("review.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
