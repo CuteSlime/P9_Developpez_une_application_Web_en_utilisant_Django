0d7896056ee7c6bf08dcaf784794400666271b78
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2024-02-21 20:07:56 +0100

    update redirection if not logged

--- LITRevu/LITRevu/settings.py
+++ LITRevu/LITRevu/settings.py
@@ -138,7 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
+LOGIN_URL = 'home'
 LOGOUT_REDIRECT_URL = "home"
+LOGOUT_URL = 'home'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
